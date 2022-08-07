2b897166c6a40692bfdc690a1cfc43232fa2bec9
flybirdgroup <53985730+flybirdgroup@users.noreply.github.com>
2022-08-07 08:07:40 +0800

    update website dependency

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/flybird/Desktop/my-website/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/flybird/Desktop/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/flybird/Desktop/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/flybird/Desktop/my-website/src/css/custom.css'),
+];
